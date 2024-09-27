fb522e3d72cfa0f13e22afa0a8cdc4b3a1739d47
kelly1210 <dongkeke89@gmail.com>
2024-09-27 17:44:33 +0800

    Add files via upload

---
+++ bin/offDNA/DEL_predict/work.sh
@@ -0,0 +1,8 @@
+python DEL_predict_v12.py \
+-s /home/data1/Run_script/NGS064_20230331_kechuang_S2_L004_nova/sample_info.txt \
+-i1 /home/data1/Analysis/NGS064_20230331_kechuang_S2_L004_nova/09.report/table/NGS064_20230331_kechuang_S2_L004_nova.count.sum.dlp.detail.txt \
+-i2 /home/data1/Analysis/NGS064_20230331_kechuang_S2_L004_nova/06.Dwar \
+-m ./models/model17/RF.pkl \
+-o ./NGS064_20230331_kechuang_S2_L004_nova.count.sum.dlp.detail.confirmation.txt
+
+
