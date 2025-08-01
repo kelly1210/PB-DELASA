27fc061e4e31187eec2ea34f6c4b6bbbe3f79cf9
kelly1210 <dongkeke89@gmail.com>
2025-08-01 14:27:34 +0800

    Delete bin directory

--- bin/monitor/work.sh
+++
@@ -1,9 +0,0 @@
-#~/bin/sh
-
-python pyflow_DEL-v.py --caseFq $fastqPATH/R1.fastq.gz,$fastqPATH/R2.fastq.gz \
--c $configPATH/config${i}.txt \
--p PBDEL-00${i} \
--o $outpath
-
-
-
--- bin/offDNA/README.txt
+++
@@ -1,6 +0,0 @@
-offDNAenu:
-Required Files: 
-1. {}/06.Dwar/{}/{}/{}.count.freq.filter.txt
-2. dlp file
-
-
